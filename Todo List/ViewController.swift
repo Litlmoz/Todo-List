b4ce0dd27d81cddd11d3ed6ba23a16796c7219bc
David Solis <litlmoz@gmail.com>
2018-08-02 16:06:04 -0700

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by David Solis on 8/2/18.
+//  Copyright © 2018 Peaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
