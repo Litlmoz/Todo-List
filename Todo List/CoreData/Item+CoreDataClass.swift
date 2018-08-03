260d16ba9a4f350b9f11ad48ed45ef714fc7d6f8
David Solis <litlmoz@gmail.com>
2018-08-02 17:24:50 -0700

    add ManagedObject Subclass

---
+++ Todo List/CoreData/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  Todo List
+//
+//  Created by David Solis on 8/2/18.
+//  Copyright © 2018 Peaking. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ Todo List/CoreData/Item+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Item+CoreDataProperties.swift
+//  Todo List
+//
+//  Created by David Solis on 8/2/18.
+//  Copyright © 2018 Peaking. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var isCompleted: Bool
+    @NSManaged public var text: String?
+
+}
