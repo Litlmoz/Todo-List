a8839474a096cda66090507b26492076a9725bf9
David Solis <litlmoz@gmail.com>
2018-08-03 16:07:41 -0700

    create singleton of managedObjectContext

--- Todo List/Supporting Files/AppDelegate.swift
+++ Todo List/Supporting Files/AppDelegate.swift
@@ -13,6 +13,21 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Todo_List")
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        
+        return container
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,32 +55,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        saveContext()
+        managedObjectContext.saveChanges()
     }
+}
 
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Todo_List")
-        container.loadPersistentStores(completionHandler: { _, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    private func saveContext () {
-        let context = persistentContainer.viewContext
-        
-        if context.hasChanges {
+extension NSManagedObjectContext {
+    
+    /// CoreData saving support
+    func saveChanges() {
+        if self.hasChanges {
             do {
-                try context.save()
-            } catch let error as NSError {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                try save()
+            } catch {
+                fatalError(error.localizedDescription)
             }
         }
     }
