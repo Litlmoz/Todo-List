a1d6ebeb7caf2fe8cd94d7cbe6daf3e5e46bf319
David Solis <litlmoz@gmail.com>
2018-08-02 18:38:05 -0700

    remove CoreDataStack comments

--- Todo List/CoreData/CoreDataStack.swift
+++ Todo List/CoreData/CoreDataStack.swift
@@ -12,12 +12,6 @@ import CoreData
 class CoreDataStack {
     
     lazy var managedObjectContext: NSManagedObjectContext = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
         let container = self.persistentContainer
         
         return container.viewContext
@@ -27,14 +21,6 @@ class CoreDataStack {
         let container = NSPersistentContainer(name: "Todo_List")
         container.loadPersistentStores() { storeDescription, error in
             if let error = error as NSError? {
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error: \(error), \(error.userInfo)")
             }
         }
