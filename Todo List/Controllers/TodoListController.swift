a821e130a9fd25445c2b9510c8b18a5e299a8094
David Solis <litlmoz@gmail.com>
2018-08-02 18:33:34 -0700

    use FetchedResultsController to display items in TodoListController

--- Todo List/Controllers/TodoListController.swift
+++ Todo List/Controllers/TodoListController.swift
@@ -12,19 +12,20 @@ import CoreData
 class TodoListController: UITableViewController {
     
     let managedObjectContext = CoreDataStack().managedObjectContext
-    var items = [Item]() {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    lazy var fetchedResultsController: NSFetchedResultsController<Item> = {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let controller = NSFetchedResultsController(fetchRequest: request,
+                                                    managedObjectContext: managedObjectContext,
+                                                    sectionNameKeyPath: nil,
+                                                    cacheName: nil)
+        return controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        
         do {
-            items = try managedObjectContext.fetch(request)
+            try fetchedResultsController.performFetch()
         } catch {
             fatalError(error.localizedDescription)
         }
@@ -33,16 +34,24 @@ class TodoListController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return fetchedResultsController.sections?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        guard let section = fetchedResultsController.sections?[section] else {
+            return 0
+        }
+        return section.numberOfObjects
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        let item = items[indexPath.row]
+        
+        return configureCell(cell, at: indexPath)
+    }
+    
+    private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
+        let item = fetchedResultsController.object(at: indexPath)
         
         cell.textLabel?.text = item.text
         
