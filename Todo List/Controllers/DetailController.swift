df5e9ffa299649cdfc1689f123376d458bf5c3fd
David Solis <litlmoz@gmail.com>
2018-08-06 16:26:03 -0700

    update UI when keyboard appears

--- Todo List/Controllers/DetailController.swift
+++ Todo List/Controllers/DetailController.swift
@@ -11,14 +11,24 @@ import CoreData
 
 class DetailController: UIViewController {
     
+    @IBOutlet weak var textFieldBottomAnchor: NSLayoutConstraint!
+    @IBOutlet weak var textFieldCenterY: NSLayoutConstraint!
     @IBOutlet weak var detailTextField: UITextField!
     var item: Item?
     var context: NSManagedObjectContext?
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        registerForKeyboardNotifications()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         if let item = item {
             detailTextField.text = item.text
         }
+    }
     }
     
     @IBAction func save(_ sender: Any) {
@@ -35,6 +45,34 @@ class DetailController: UIViewController {
             context?.saveChanges()
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let info = notification.userInfo as NSDictionary?,
+            let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size else {
+                return
+        }
+        textFieldBottomAnchor.constant = keyboardSize.height
+        textFieldCenterY.isActive = false
+        textFieldBottomAnchor.isActive = true
+        view.layoutIfNeeded()
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        textFieldBottomAnchor.isActive = false
+        textFieldCenterY.isActive = true
+        view.layoutIfNeeded()
     }
 }
 
